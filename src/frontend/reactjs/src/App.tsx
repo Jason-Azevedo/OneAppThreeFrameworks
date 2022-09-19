2138deeb0273c1c480f7d1318bf81bfa0d5d183e
Jason Azevedo <jasonazevedo03@gmail.com>
2022-09-19 22:05:38 +0200

    Setup routing

--- src/frontend/reactjs/src/App.tsx
+++ src/frontend/reactjs/src/App.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
+
+import "./sass/main.scss";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Outlet />}>
+          <Route index element={<h1>This is the home page</h1>} />
+          <Route path="/post" element={<h1>This is the post page</h1>} />
+
+          {/* 
+            <AuthRoutes>
+              <Route path="/me">
+                <Route index /> 
+                <Route path="/posts">
+                  <Route index />
+                  <Route path="/new" /> 
+                  <Route path="/edit" />
+                </Route>
+                 
+                <Route path="/settings" />
+              </Route> 
+            </ AuthRoutes>
+          */}
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
