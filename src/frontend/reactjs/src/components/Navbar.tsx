679402af9cc0ea3e32565d2231ec8fabd5935a5a
Jason Azevedo <jasonazevedo03@gmail.com>
2022-09-20 23:15:25 +0200

    Added fontawesome to the project

--- src/frontend/reactjs/src/components/Navbar.tsx
+++ src/frontend/reactjs/src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
   return (
-    <div className="navbar container--600 flex-row justify-sb">
+    <div className="navbar container--600 flex-row justify-sb align-c">
       <h3>O.A.T.F.</h3>
 
-      <span>Home Icon</span>
+      <FontAwesomeIcon icon={faHome} />
 
       <span>Other Icons</span>
     </div>
