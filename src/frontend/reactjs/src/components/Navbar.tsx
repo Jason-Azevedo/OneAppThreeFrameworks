c568dee4a518e89f6c058f35e26163c4e18bf344
Jason Azevedo <jasonazevedo03@gmail.com>
2022-09-21 14:01:12 +0200

    Styles the navbar icons

--- src/frontend/reactjs/src/components/Navbar.tsx
+++ src/frontend/reactjs/src/components/Navbar.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faBell, faUser } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div className="navbar container--600 flex-row justify-sb align-c">
-      <h3>O.A.T.F.</h3>
+      <h3 className="title--18 color-primary">O.A.T.F.</h3>
 
-      <FontAwesomeIcon icon={faHome} />
+      <Link to={"/"}>
+        <FontAwesomeIcon icon={faHome} className="icon--18" />
+      </Link>
 
-      <span>Other Icons</span>
+      {/* Display this menu if the user signed in */}
+      <div className="flex-row justify-sb align-c gap--24">
+        <FontAwesomeIcon icon={faBell} className="icon--18" />
+
+        {/* Display user profile picture if the uploaded one */}
+        <FontAwesomeIcon icon={faUser} className="icon--18" />
+      </div>
     </div>
   );
 }
